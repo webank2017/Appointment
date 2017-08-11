51416455a9c93d47635fe46f39cb79245ecfe7f1
wqliang <384598740@qq.com>
2017-08-11 20:03:44 +0800

    用户登录逻辑

---
+++ src/main/java/com/webank/Appointment/service/UserService.java
@@ -0,0 +1,9 @@
+package com.webank.Appointment.service;
+
+import com.webank.Appointment.module.PersonInfo;
+
+public interface UserService {
+	boolean addUser(PersonInfo user);
+	boolean isExist(String openid);
+
+}
